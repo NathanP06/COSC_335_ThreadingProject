e3ff24a977032d2068aacfa543d6594214440a8e
NathanP06 <nathanjpugh6@gmail.com>
2025-09-18 12:40:46 -0500

    Added comments to code to explain

--- ThreadingProject/Program.cs
+++ ThreadingProject/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 class Program
 {
+    //Method to print numbers
     static void PrintingNumbers(string name, int start, int end)
     {
         for (int i = start; i <= end; i++)
@@ -14,40 +15,47 @@ class Program
         }
     }
 
+    //Running without syncronization (All threads run together)
     static void ThreadingWithoutSyncNumbers()
     {
-        //Running without syncronization (All threads run together) 
+        //Creates 5 threads
         Thread t1 = new Thread(() => PrintingNumbers("Thread #1", 1, 10));
         Thread t2 = new Thread(() => PrintingNumbers("Thread #2", 11, 20));
         Thread t3 = new Thread(() => PrintingNumbers("Thread #3", 21, 30));
         Thread t4 = new Thread(() => PrintingNumbers("Thread #4", 31, 40));
         Thread t5 = new Thread(() => PrintingNumbers("Thread #5", 41, 50));
 
+        //Starts all five threads
         t1.Start();
         t2.Start();
         t3.Start();
         t4.Start();
         t5.Start();
 
+        //Waits for threads to complete before continuing
         t1.Join();
         t2.Join();
         t3.Join();
         t4.Join();
         t5.Join();
 
+        //Prints "done" to inform user that tasks have been completed
         Console.WriteLine("Done!");
     }
-
-    static void ThreadingWithSyncNumbers(){
-        //Running with syncronization/in order (one thread at a time)
+    
+    //Running with syncronization/in order (one thread at a time)
+    static void ThreadingWithSyncNumbers()
+    {
+        //Creates 5 threads
         Thread t6 = new Thread(() => PrintingNumbers("Thread #1", 1, 10));
         Thread t7 = new Thread(() => PrintingNumbers("Thread #2", 11, 20));
         Thread t8 = new Thread(() => PrintingNumbers("Thread #3", 21, 30));
         Thread t9 = new Thread(() => PrintingNumbers("Thread #4", 31, 40));
         Thread t10 = new Thread(() => PrintingNumbers("Thread #5", 41, 50));
 
+        //Starts thread, waits for it to finish, then starts next thread
         t6.Start();
-        t6.Join(); 
+        t6.Join();
 
         t7.Start();
         t7.Join();
@@ -61,7 +69,7 @@ class Program
         t10.Start();
         t10.Join();
 
-
+        //Prints "done" to inform user that tasks have been completed
         Console.WriteLine("Done!");
     }
 
@@ -73,14 +81,20 @@ class Program
     static void Main()
     {
         // Measure ThreadingWithoutSyncNumbers (milliseconds)
+            //Creates stopwatch object, runs method, then stops the stop watch)
         var sw1 = Stopwatch.StartNew();
         ThreadingWithoutSyncNumbers();
         sw1.Stop();
+
+        //Stores value in variable ms
         double ms = sw1.Elapsed.TotalMilliseconds;
+
+        //prints elapsed time to console
         Console.WriteLine($"ThreadingWithoutSyncNumbers elapsed: {ms:F3} ms");
         Console.WriteLine("--------------------------------------------------");
 
         // Measure ThreadingWithSyncNumbers (milliseconds)
+            //Does the same as above but for the other method
         var sw2 = Stopwatch.StartNew();
         ThreadingWithSyncNumbers();
         sw2.Stop();
